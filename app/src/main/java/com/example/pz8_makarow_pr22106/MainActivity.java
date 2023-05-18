1a62bcc863d09e077c4bd4d055d21ae9385b81bb
egor <porem.578@gmail.com>
2023-05-19 00:40:29 +0700

    2

--- app/src/main/java/com/example/pz8_makarow_pr22106/MainActivity.java
+++ app/src/main/java/com/example/pz8_makarow_pr22106/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.pz8_makarow_pr22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sw = (Button) findViewById(R.id.button2);
+        View.OnClickListener OCL = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+            }
+        };
+        button2.setOnClickListener(sw);
     }
 }
--- app/src/main/java/com/example/pz8_makarow_pr22106/MainActivity2.java
+++ app/src/main/java/com/example/pz8_makarow_pr22106/MainActivity2.java
@@ -2,13 +2,25 @@ package com.example.pz8_makarow_pr22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class MainActivity2 extends AppCompatActivity {
-
+Button sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        sw = (Button) findViewById(R.id.button);
+        View.OnClickListener oCL = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent();
+            }
+        };
     }
 }
